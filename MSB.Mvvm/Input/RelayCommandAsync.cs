1cdce33ca0797a4e9ddc626c525bfdb8a3d45bb2
jordy-ricardo25 <jordy.ricardo25@outlook.com>
2023-07-02 10:34:31 -0500

    Added 'RelayCommandAsync' and 'RelayCommandAsync<T>' types.

---
+++ MSB.Mvvm/Input/RelayCommandAsync.cs
@@ -0,0 +1,93 @@
+﻿using System.Threading.Tasks;
+using System.Windows.Input;
+using System;
+
+namespace MSB.Mvvm.Input
+{
+    /// <summary>
+    /// A command whose sole purpose is to relay its functionality to other
+    /// objects by invoking delegates.
+    /// </summary>
+    public sealed class RelayCommandAsync : ICommand
+    {
+        /// <summary>
+        /// Initializes a new instance of the RelayCommand class that can always execute.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        public RelayCommandAsync(Func<Task> execute) : this(execute, null)
+        {
+            
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the RelayCommand class.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        /// <param name="canExecute">The execution status logic.</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public RelayCommandAsync(Func<Task> execute, Func<bool>? canExecute)
+        {
+            if (execute is null)
+                throw new ArgumentNullException(nameof(execute));
+
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        #region Methods
+
+        /// <inheritdoc/>
+        public bool CanExecute(object parameter)
+        {
+            return !isExecuting && canExecute?.Invoke() is true;
+        }
+
+        /// <inheritdoc/>
+        public async void Execute(object parameter)
+        {
+            isExecuting = true;
+
+            try
+            {
+                if (CanExecute(parameter))
+                    await execute();
+            }
+            finally
+            {
+                isExecuting = false;
+            }
+        }
+
+        /// <summary>
+        /// Raises the <see cref="CanExecuteChanged"/> event.
+        /// </summary>
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        #endregion
+
+        #region Events
+
+        /// <inheritdoc/>
+        public event EventHandler? CanExecuteChanged;
+
+        #endregion
+
+        /// <summary>
+        /// The action to invoke when <see cref="Execute"/> is used.
+        /// </summary>
+        readonly Func<Task> execute;
+
+        /// <summary>
+        /// The optional action to invoke when <see cref="CanExecute"/> is used.
+        /// </summary>
+        readonly Func<bool>? canExecute;
+
+        /// <summary>
+        /// A flag indicating whether the command is currently executing.
+        /// </summary>
+        bool isExecuting;
+    }
+}
